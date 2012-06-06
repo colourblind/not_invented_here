4ffac92e0f4406bd26a05fe0d64314bb8fc0bcfc
colourblind <tom.milsom@gmail.com>
2012-06-06 20:50:50 +0100

    Batch files to avoid messing around in the REPL

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -run not_invented_here start
+if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -run not_invented_here start
