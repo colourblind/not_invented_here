f02f5036757b448168754297cb8a79b1e118b9be
colourblind <tom.milsom@gmail.com>
2012-06-11 22:06:13 +0100

    The tidyup begins . . .

--- irc.erl
+++ irc.erl
@@ -45,13 +45,21 @@ send_to_channel(Pid, Sender, RecipientChannel, Message) ->
             lists:foreach(fun(User) -> client_handler:send_message(User#user.clientPid, FinalMessage) end, UserList)
     end.
     
+send_raw_to_channel(Pid, RecipientChannel, Message) ->
+    case state:get_channel(Pid, RecipientChannel) of
+        false ->
+            io:format("CANNOT FIND RECIPIENT ~p~n", [RecipientChannel]);
+        Channel ->
+            UserList = lists:map(fun(Nick) -> state:get_user_by_nick(Pid, Nick) end, Channel#channel.users),
+            lists:foreach(fun(User) -> client_handler:send_message(User#user.clientPid, Message) end, UserList)
+    end.
+    
 join_channel(Pid, SenderPid, ChannelName) ->
     Sender = state:get_user_by_pid(Pid, SenderPid),
     Channel = state:join_channel(Pid, ChannelName, Sender),
     Prefix = ":" ++ Sender#user.nick ++ "!" ++ Sender#user.username ++ "@" ++ Sender#user.clientHost,
     FinalMessage = Prefix ++ " JOIN " ++ " :"  ++ Channel#channel.name ++ "\r\n",
-    UserList = lists:map(fun(Nick) -> state:get_user_by_nick(Pid, utils:normalise_nick(Nick)) end, Channel#channel.users),
-    lists:foreach(fun(User) -> client_handler:send_message(User#user.clientPid, FinalMessage) end, UserList),
+    send_raw_to_channel(Pid, ChannelName, FinalMessage),
     client_handler:send_message(Sender#user.clientPid, ":" ++ ?SERVER_NAME ++ " 353 " ++ Sender#user.nick ++ " " ++ ChannelName ++ " :" ++ string:join(Channel#channel.users, " ") ++ "\r\n"),
     client_handler:send_message(Sender#user.clientPid, ":" ++ ?SERVER_NAME ++ " 366 " ++ Sender#user.nick ++ " " ++ ChannelName ++ " :End of /NAMES list.\r\n"),
     ok.
