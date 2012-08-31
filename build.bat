f52ee3c3e582ccb3fadb884c15438919c46d1c13
colourblind <tom.milsom@gmail.com>
2012-08-31 22:54:07 +0100

    Moved to a more reasonable directory structure

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
-if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erlc" not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl
-if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erlc" not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl
+pushd src
+if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erlc" -o ..\ebin -I ..\include not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl
+if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erlc" -o ..\ebin -I ..\include not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl
+popd
