4f5bdc6e76b6e771aabbbf13968f8468e3db489f
colourblind <tom.milsom@gmail.com>
2012-09-03 23:18:30 +0100

    Using -pa in run script rather than mess around with pushd/popd

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
 @echo off
-pushd ebin
-if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -run not_invented_here start
-if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -run not_invented_here start
-popd
+if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -pa ebin -run not_invented_here start
+if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -pa ebin -run not_invented_here start
