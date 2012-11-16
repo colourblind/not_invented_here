711ba0877a43cd23c38ce0b2f38be454c6a04fe7
colourblind <tom.milsom@gmail.com>
2012-11-16 21:49:21 +0000

    Added -noshell to run script

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -pa ebin -run not_invented_here start
-if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -pa ebin -run not_invented_here start
+if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -pa ebin -noshell -run not_invented_here start
+if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -pa ebin -noshell -run not_invented_here start
