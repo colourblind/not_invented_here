5e50c0ad1a2325bd01dc82ef493624d4a26cc1e3
colourblind <tom.milsom@gmail.com>
2013-01-20 22:02:25 +0000

    Fixed broken build (derp). Removed some spurious parameters

--- src/client_handler.erl
+++ src/client_handler.erl
@@ -31,7 +31,7 @@ handle_info({tcp, Socket, Data}, State) ->
     io:format("handle_info TCP ~p ~p~n", [Socket, Data]),
     Params = utils:get_client_params(Data),
     Command = utils:get_client_command(Data),
-    {noreply, handle_command(Command, Params, State, Socket)};
+    {noreply, handle_command(Command, Params, State)};
 handle_info(send_ping, State) ->
     gen_tcp:send(element(2, State), "PING :" ++ ?SERVER_NAME ++ "\r\n"),
     erlang:send_after(?PING_TIMEOUT, self(), ping_timeout),
@@ -39,15 +39,15 @@ handle_info(send_ping, State) ->
 handle_info(ping_timeout, State) ->
     case element(3, State) of
         true ->
-            handle_command("QUIT", ["Ping timeout"], State, Socket),
+            handle_command("QUIT", ["Ping timeout"], State),
             gen_tcp:close(element(2, State)),
             gen_server:cast(self(), stop);
         false ->
             ok
     end,
     {noreply, State};
-handle_info({tcp_closed, Socket}, State) ->
-    handle_command("QUIT", ["Connection reset by peer"], State, Socket),
+handle_info({tcp_closed, _}, State) ->
+    handle_command("QUIT", ["Connection reset by peer"], State),
     {stop, normal, State}.
 
 terminate(Reason, State) ->
@@ -58,11 +58,10 @@ code_change(OldVsn, State, Extra) ->
     io:format("code_change ~p ~p with state ~p~n", [OldVsn, Extra, State]),
     {ok, State}.
     
-handle_command("PONG", _, State, Socket) ->
-    io:format("Received client PONG \\n ~p~n", [Socket]),
+handle_command("PONG", _, State) ->
     erlang:send_after(?PING_INTERVAL, self(), send_ping),
     setelement(3, State, false);
-handle_command(Command, Params, State, Socket) ->
+handle_command(Command, Params, State) ->
     case Command of
         "PRIVMSG" ->
             irc:privmsg(element(1, State), self(), lists:nth(1, Params), lists:nth(2, Params));
@@ -88,7 +87,7 @@ handle_command(Command, Params, State, Socket) ->
             irc:whois(element(1, State), self(), lists:nth(1, Params));
         "QUIT" ->
             irc:quit(element(1, State), self(), lists:nth(1, Params)),
-            gen_tcp:close(Socket),
+            gen_tcp:close(element(2, State)),
             gen_server:cast(self(), stop);
         "PING" ->
             io:format("Reponding to client PING~n"),
