cdab5608f1b0bce80ba198cb0cccb68b5e1b057d
colourblind <tom.milsom@gmail.com>
2013-03-16 20:33:20 +0000

    So I just learned about emake. That makes things easier

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-pushd src
-if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erlc" -o ..\ebin -I ..\include not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl handshake.erl
-if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erlc" -o ..\ebin -I ..\include not_invented_here.erl irc.erl client_handler.erl utils.erl state.erl handshake.erl
-popd
+if exist "%ProgramFiles(x86)%" "%ProgramFiles(x86)%\erl5.9\bin\erl" -make
+if not exist "%ProgramFiles(x86)%" "%ProgramFiles%\erl5.9\bin\erl" -make
